6169254a54727a30b43fe1e94deedaad01cd4904
Nicholas Newberry <nn00020@my.westga.edu>
2025-02-01 14:28:52 -0500

    Reorganize Project Structure
    - Create /code folder and move project into it
    - Create /documentation folder
    - Create /sprints folder

---
+++ code/TaskManager/TaskManagerDatabase/Script.PostDeployment1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO [User] AS Target
+USING (VALUES ('admin', 'admin', 'Admin')) AS Source (Username, Password, Role)
+ON Target.Username = Source.Username
+WHEN MATCHED THEN
+    UPDATE SET Password = Source.Password, Role = Source.Role
+WHEN NOT MATCHED THEN
+    INSERT (Username, Password, Role)
+    VALUES (Source.Username, Source.Password, Source.Role); 
+
+
---
+++ code/TaskManager/TaskManagerWebsite/Models/Metadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerWebsite.Models
+{
+    public class UserMetadata
+    {
+        [StringLength(10, MinimumLength = 5)]
+        public string Username;
+
+        [StringLength(10, MinimumLength = 5)]
+        public string Password;
+    }
+}
