ec18270ea042d215178e728f309f0fa8fcc2cf2e
Trinidad Mario Dena <tdena1@my.westga.edu>
2025-02-08 15:11:50 -0500

    Merged Changes And Working Project

---
+++ code/LoginControlLibrary/LoginControlLibrary/UserControl1.cs
@@ -0,0 +1,10 @@
+﻿namespace LoginControlLibrary
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
