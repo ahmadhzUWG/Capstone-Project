10712e90434439c55648869105d03aa5033c8e4d
Nicholas Newberry <nn00020@my.westga.edu>
2025-02-10 16:01:56 -0500

    Commit files necessary to pull
    Commit backend autogenerated files to resolve conflicts in order to pull

--- code/TaskManager/obj/Debug/net8.0/TaskManagerWebsite.AssemblyInfo.cs
+++ code/TaskManager/obj/Debug/net8.0/TaskManagerWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb704d31faeca9a66df89f857a823ae805c8d4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
