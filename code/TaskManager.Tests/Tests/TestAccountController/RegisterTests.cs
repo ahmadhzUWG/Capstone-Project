504c8d5e6b20bb16e51bfa726b5a12b5b3c131ba
Nicholas Newberry <nn00020@my.westga.edu>
2025-02-10 18:11:47 -0500

    Implement Testing Frameworks
    -Add xUnit testing frameworks to solution
    - Create xUnit Test project to house all Unit Test files

---
+++ code/TaskManager.Tests/Tests/TestAccountController/RegisterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.ContentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagerWebsite.Controllers;
+using TaskManagerWebsite.Data;
+using TaskManagerWebsite.Models;
+using TaskManagerWebsite.ViewModels;
+using Xunit;
+
+public class RegisterTests
+{
+    private ApplicationDbContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        var dbContext = new ApplicationDbContext(options);
+        return dbContext;
+    }
+
+}
