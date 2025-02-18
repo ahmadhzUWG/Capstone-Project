b3bfb43aba5ff927aedd134dc0064bb47de4067e
Trinidad Mario Dena <tdena1@my.westga.edu>
2025-02-18 11:50:21 -0500

    Added cleanup script Used to remove unnecessary bin and obj dirs

---
+++ clean_build_dirs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# List of directories to remove
+DIRS=(
+    "code/TaskManagerDesktop/obj/"
+    "code/TaskManagerDesktop/bin/"
+    "code/TaskManager/bin/"
+    "code/TaskManager/obj/"
+    "code/TaskManager/bin/"
+    "code/TaskManager.Tests/bin/"
+    "code/TaskManager.Tests/obj/"
+)
+
+# Loop through and remove each directory
+for dir in "${DIRS[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "Deleting: $dir"
+        rm -rf "$dir"
+    else
+        echo "Skipping: $dir (not found)"
+    fi
+done
+
+echo "Cleanup complete!"
