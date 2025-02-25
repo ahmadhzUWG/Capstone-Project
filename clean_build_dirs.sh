d31f334d8d19e85e91529da593cdeb56e021d337
Trinidad Mario Dena <tdena1@my.westga.edu>
2025-02-25 12:51:53 -0500

    Updated Cleanup Script now removes user specific generated file file generated from Resharper, [ProjectName].sln.DotSettings.user

--- clean_build_dirs.sh
+++ clean_build_dirs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# List of directories to remove
-DIRS=(
+# List of paths (both directories and files) to remove
+DIRS_AND_FILES=(
     "code/TaskManagerDesktop/obj/"
     "code/TaskManagerDesktop/bin/"
     "code/TaskManager/bin/"
@@ -12,13 +12,27 @@ DIRS=(
     "code/.vs/"
 )
 
+FILES=(
+    "code/G4CapstoneProject.sln.DotSettings.user"
+)
+
 # Loop through and remove each directory
-for dir in "${DIRS[@]}"; do
+for dir in "${DIRS_AND_FILES[@]}"; do
     if [ -d "$dir" ]; then
-        echo "Deleting: $dir"
+        echo "Deleting directory: $dir"
         rm -rf "$dir"
     else
         echo "Skipping: $dir (not found)"
+    fi
+done
+
+# Loop through and remove each file
+for file in "${FILES[@]}"; do
+    if [ -f "$file" ]; then
+        echo "Deleting file: $file"
+        rm -f "$file"
+    else
+        echo "Skipping: $file (not found)"
     fi
 done
 
