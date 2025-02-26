f3f595c54906e30c185f9864008721d99c7db351
Nicholas Newberry <nn00020@my.westga.edu>
2025-02-26 17:56:07 -0500

    Clean Model Classes
    - Add specifications to classes and fields
    -Remove unused directives in each file

--- code/TaskManager/Models/GroupRequest.cs
+++ code/TaskManager/Models/GroupRequest.cs
@@ -1,13 +1,44 @@
-﻿namespace TaskManagerWebsite.Models
+﻿namespace TaskManagerWebsite.Models;
+
+/// <summary>
+/// Represents a request made by a user to assign a group to a project
+/// This request may be accepted or rejected.
+/// </summary>
+public class GroupRequest
 {
-    public class GroupRequest
-    {
-        public int Id { get; set; }
-        public int SenderId { get; set; } 
-        public int GroupId { get; set; }     
-        public Group Group { get; set; }
-        public int ProjectId { get; set; }
-        public Project Project { get; set; }
-        public bool? Response { get; set; }  
-    }
+    /// <summary>
+    /// Gets or sets the unique identifier for the group request.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the user who sent the request.
+    /// </summary>
+    public int SenderId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the group to which the request is made.
+    /// </summary>
+    public int GroupId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the group entity associated with the request.
+    /// </summary>
+    public Group Group { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the project that is being requested for association with the group.
+    /// </summary>
+    public int ProjectId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the project entity associated with the request.
+    /// </summary>
+    public Project Project { get; set; }
+
+    /// <summary>
+    /// Gets or sets the response status of the request.
+    /// A value of <c>true</c> indicates acceptance, <c>false</c> indicates rejection, and <c>null</c> means pending.
+    /// </summary>
+    public bool? Response { get; set; }
 }
--- code/TaskManager/Models/User.cs
+++ code/TaskManager/Models/User.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace TaskManagerWebsite.Models;
 
+/// <summary>
+/// Represents a user in the task management system.
+/// Inherits from <see cref="IdentityUser{TKey}"/> to integrate with ASP.NET Identity.
+/// </summary>
 public class User : IdentityUser<int>
 {
+    /// <summary>
+    /// Gets or sets the collection of groups the user is a member of.
+    /// A user can belong to multiple groups.
+    /// </summary>
     public ICollection<Group> Groups { get; set; } = new List<Group>();
 }
