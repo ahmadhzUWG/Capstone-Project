ca6c347daab025f902bb05378a23b126fd1deb2f
Trinidad Mario Dena <tdena1@my.westga.edu>
2025-03-12 15:52:23 -0400

    Updated ViewModel Required Fields Added required statement to prevent runtime null references

--- code/TaskManager/ViewModels/UserDeleteViewModel.cs
+++ code/TaskManager/ViewModels/UserDeleteViewModel.cs
@@ -4,8 +4,8 @@ namespace TaskManagerWebsite.ViewModels
 {
     public class UserDeleteViewModel
     {
-        public User User { get; set; }
-        public List<Group> RelatedGroups { get; set; }
-        public List<Project> RelatedProjects { get; set; }
+        public required User User { get; set; }
+        public required List<Group> RelatedGroups { get; set; }
+        public required List<Project> RelatedProjects { get; set; }
     }
 }
